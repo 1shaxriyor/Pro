99a200de97f55279397647833201db31eb2b78aa
Your name <killerabdulaziz@gmail.com>
2025-11-19 15:04:04 +0500

    write service html

--- services/views.py
+++ services/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Service, Master
 
-# Create your views here.
+def services_list(request):
+    services = Service.objects.all()
+    return render(request, "service/service_list.html", {"services": services})
+
+def service_detail(request, slug):
+    service = get_object_or_404(Service, slug=slug)
+    masters = Master.objects.filter(service=service)
+    return render(request, "service/service_detail.html", {"service": service, "masters": masters})
